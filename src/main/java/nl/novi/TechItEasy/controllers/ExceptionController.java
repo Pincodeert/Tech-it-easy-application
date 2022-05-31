109c679626f18402995821d25793e3297772e0c9
Pincodeert <leonie.pin@gmail.com>
2022-05-31 15:59:13 +0200

    ExceptionController gemaakt

--- src/main/java/nl/novi/TechItEasy/controllers/ExceptionController.java
+++ src/main/java/nl/novi/TechItEasy/controllers/ExceptionController.java
@@ -1,4 +1,18 @@
 package nl.novi.TechItEasy.controllers;
 
+import nl.novi.TechItEasy.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(value = (RecordNotFoundException.class))
+    public ResponseEntity<Object> exception(RecordNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 }
