6cdaab4034c668a1a8943fa035f4e671a5e34f6b
Pincodeert <leonie.pin@gmail.com>
2022-05-31 14:56:58 +0200

    RecordNotFoundException uitgewerkt

--- src/main/java/nl/novi/TechItEasy/exceptions/RecordNotFoundException.java
+++ src/main/java/nl/novi/TechItEasy/exceptions/RecordNotFoundException.java
@@ -1,5 +1,14 @@
 package nl.novi.TechItEasy.exceptions;
 
-public class RecordNotFoundException {
+public class RecordNotFoundException extends RuntimeException{
 
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException(){
+        super();
+    }
+
+    public RecordNotFoundException(String message){
+        super(message);
+    }
 }
